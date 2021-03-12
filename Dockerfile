a6dbd0bdd1f2af0ddc496fd1dc5ad9dc2333b2ac
Matt <37265551+MattyTheDeveloper@users.noreply.github.com>
2021-03-12 23:22:28 +0000

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,20 @@ ENV PORT 25570
 
 # DOWNOAD SHIT
 RUN apt-get clean
+RUN apt-get install unzip
 
 
 # SORTS OUT FILES
 COPY spigot.yml /tmp/spigot.yml
+COPY ops.json /tmp/ops.json
 COPY start.sh /start.sh
-COPY server.properties /server.properties
+
+COPY /plugins/DeniableLobby.jar /tmp/plugins/DeniableLobby.jar
+COPY /plugins/LuckPerms.jar /tmp/plugins/LuckPerms.jar
+COPY /plugins/Vault.jar /tmp/plugins/Vault.jar
+
+COPY ./plugins/Lobby/. /tmp/plugins/Lobby
+COPY ./plugins/LuckPerms/. /tmp/plugins/LuckPerms
 RUN chmod 755 /start.sh
 
 # USER MANAGEMENT
--- start.sh
+++ start.sh
@@ -12,13 +12,36 @@ if [ ! -e /data/spigot.yml ]; then
 	cp /tmp/spigot.yml /data/spigot.yml
 fi
 
+if [ ! -e /data/ops.json ]; then
+	cp /tmp/ops.json /data/ops.json
+fi
+
 if [ ! -e /data/server.properties ]; then
 	wget -q -O /data/server.properties https://www.dropbox.com/s/4cr2nd1l6236e71/server.properties?dl=1
+	echo "server-port=$PORT" >> /data/server.properties
+	echo "online-mode=false" >> /data/server.properties
 fi
+
+if [ ! -e /data/world ]; then
+	wget -q -O /data/world.zip https://www.dropbox.com/s/79kn0mhzc3p85h2/world.zip?dl=1
+	unzip world.zip -d /data/
+fi
+
+if [ ! -e /data/plugins/DeniableLobby.jar ]; then
+	mkdir /data/plugins
+	mkdir /data/plugins/Lobby
+	mkdir /data/plugins/LuckPerms
+	cp /tmp/plugins/DeniableLobby.jar /data/plugins/DeniableLobby.jar
+	cp /tmp/plugins/LuckPerms.jar /data/plugins/LuckPerms.jar
+	cp /tmp/plugins/Vault.jar /data/plugins/Vault.jar
+
+	cp /tmp/plugins/Lobby/* /data/plugins/Lobby
+	cp /tmp/plugins/LuckPerms/* /data/plugins/LuckPerms
+fi
+
 
 # Change server files
 echo "eula=true" > eula.txt
-echo "server-port=$PORT" > server.properties
 
 # Setup owner
 chown -R minecraft:minecraft /data
